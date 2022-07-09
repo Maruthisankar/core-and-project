58397c4d4246930f609970652b1b23bcfac6f34d
Maruthisankar <105190709+Maruthisankar@users.noreply.github.com>
2022-07-10 00:50:52 +0530

    Add files via upload

---
+++ Data/Final_projectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Final_project.Models;
+
+namespace Final_project.Data
+{
+    public class Final_projectContext : DbContext
+    {
+        public Final_projectContext (DbContextOptions<Final_projectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Final_project.Models.Product> Product { get; set; }
+    }
+}
