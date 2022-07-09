a82d39912e886d8cc9f813786f596e31eb79d099
Maruthisankar <105190709+Maruthisankar@users.noreply.github.com>
2022-07-10 00:54:19 +0530

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_project.Models
+{
+    public class Product
+    {
+
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string productName { get; set; }
+
+        public float Price { get; set; }
+        public string Brand { get; set; }
+        public DateTime MaufactureDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
